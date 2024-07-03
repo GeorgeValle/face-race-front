0c08a40347ec9b524660ebd58c223aeced76cd1a
GeorgeValle <jorgeguillermovalle@gmail.com>
2024-07-03 02:36:33 -0300

    (fix): change css components for styled-components

---
+++ src/components/btnVioletLarge/BtnVioletLarge.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ContainerRoboto = styled.span`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap");
+  
+    color: #000000;
+    text-align: center;
+    font-family: "Roboto";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-align: center;
+    
+`;
--- src/components/btnVioletLarge/BtnVioletLarge.jsx
+++ src/components/btnVioletLarge/BtnVioletLarge.jsx
@@ -1,17 +1,46 @@
-import StyleBtn from './BtnVioletLarge.module.css';
+// 
+import styled from 'styled-components';
+import {ContainerRoboto} from './BtnVioletLarge.js';
+
+
+const BtnViolet= ({children, onClick, btnType="button"}) => (
+    <button  onClick={onClick} type={btnType} >
+//             <ContainerRoboto>{children}</ContainerRoboto>                   
+//         </button>
+);
 
 
 
+const BtnVioletLarge = styled(BtnViolet)`
+    display: block;
+    flex-shrink: 0;
+    width:100px;
+    border-radius: 10px;
+    border: 1px solid #000000;
+    color: linear-gradient(90deg, rgba(81,97,230,1) 0%, rgba(213,81,230,1) 100%);
+    box-shadow: 6px 6px 4px 0px rgba(0, 0, 0, 0.25); 
+`
 
-const BtnVioletLarge = ({children, onClick, btnType="button"}) => {
-
-    return(
-        <>
-        <button  onClick={onclick} type={btnType} claseName={StyleBtn.buttonLarge}>
-            <span className={StyleBtn.texts}>{children}</span>                   
-        </button> 
-        </>  
-    );
-}
-
+return(
+            <>
+            <button  onClick={onclick} type={btnType} >
+                {children}                   
+            </button> 
+            </>  
+        );
+        
+        
 export default BtnVioletLarge;
+
+// const BtnVioletLarge = ({children, onClick, btnType="button"}) => {
+
+//     return(
+//         <>
+//         <button  onClick={onclick} type={btnType} claseName={StyleBtn.buttonLarge}>
+//             <span className={StyleBtn.texts}>{children}</span>                   
+//         </button> 
+//         </>  
+//     );
+// }
+
+// export default BtnVioletLarge;
