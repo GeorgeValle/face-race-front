1a5d19467dfee1eed4442daa7df0e9833dcaacaa
GeorgeValle <jorgeguillermovalle@gmail.com>
2024-07-09 18:30:06 -0300

    (feat):add view Lost_password

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import {Navigate, BrowserRouter, Route, Routes} from 'react-router-dom'
 import MainNavBar from './components/mainNavBar/MainNavBar.jsx'
 import Login from './views/auth/Login.jsx'
+import Lost_Password from './views/auth/Lost_Password.jsx'
 
 //import BodyPlane from './components/common/BodyPlane.jsx'
 // import reactLogo from './assets/react.svg'
@@ -18,6 +19,7 @@ const App = () => {
     <MainNavBar></MainNavBar>
   <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/lost_password" element={<Lost_Password/>}/>
             {/* <Route path="/" element={<HomeView/>}/>
             <Route path="/wiki_ideas_front/" element={<Navigate to="/"/>}/>
             <Route path="/articles" element={<ArticlesView/>}/> */}
@@ -25,7 +27,7 @@ const App = () => {
             <Route path="/edit/:id" element={<Edit/>}/> */}
             {/* <Route path="*" element={<HomeView/>}/> */}
             
-          </Routes>
+  </Routes>
           
   </BrowserRouter>
       
