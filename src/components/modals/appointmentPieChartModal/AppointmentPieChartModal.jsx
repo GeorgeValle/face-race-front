6dc32ab202ff888ee7383fcfcd3e01737bb241fe
GeorgeValle <jorgeguillermovalle@gmail.com>
2025-02-22 01:46:22 -0300

    (fix): error in PieChart in AppointmentPieChartModal

--- src/components/modals/appointmentPieChartModal/AppointmentPieChartModal.jsx
+++ src/components/modals/appointmentPieChartModal/AppointmentPieChartModal.jsx
@@ -1,71 +1,91 @@
-import Style from './AppointmentPieChartModal.module.css'
-import BtnClose from '../../btns/btnClose/BtnClose'
-import { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2'
+import Style from './AppointmentPieChartModal.module.css';
+import BtnClose from '../../btns/btnClose/BtnClose';
+import MiniNavBar from '../../miniNavbar/MIniNavBar';
+import {Chart as ChartJS,
+        ArcElement,
+        Tooltip,
+        Legend
+} from 'chart.js'
+import { Pie } from 'react-chartjs-2';
 
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend)
 
 const AppointmentPieChartModal = ({ appointments, onClose }) => {
 
-    const [data, setData] = useState({
-        labels: ['Pendiente', 'Cancelado', 'Ausente', 'Atendido'],
+    // const chartRef = useRef(null);
+    
+    // useEffect(() => {
+    //     if (chartRef.current) {
+    //         chartRef.current.destroy();
+    //     }
+    // }, []);
+
+    const data = {
+        labels: ['Pendientes', 'Cancelados', 'Ausentes', 'Atendidos'],
         datasets: [{
-            label: 'Estado de turnos',
-            data: [0, 0, 0, 0],
+            label: 'Estados de turnos',
+            data: [
+                appointments.filter(appointment => appointment.status === 'pending').length,
+                appointments.filter(appointment => appointment.status === 'canceled').length,
+                appointments.filter(appointment => appointment.status === 'missing').length,
+                appointments.filter(appointment => appointment.status === 'attended').length,
+            ],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
+                //'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                // 
+                'rgba(255, 165, 0, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(54, 162, 235, 1)',
             ],
             borderWidth: 1
         }]
-    });
+    };
+
+    const options = {
+        title: {
+            display: true,
+            text: 'Estados de turnos',
+        },
+        
+    };
+
+    // useEffect(() => {
+    //     setChartId(chartId + 1);
+    // }, [appointments]);
 
 
-    useEffect(() => {
-        const pendiente = appointments.filter(appointment => appointment.status === 'pending').length;
-        const cancelado = appointments.filter(appointment => appointment.status === 'canceled').length;
-        const ausente = appointments.filter(appointment => appointment.status === 'missing').length;
-        const atendido = appointments.filter(appointment => appointment.status === 'attended').length;
 
-        setData({
-            labels: ['Pendiente', 'Cancelado', 'Ausente', 'Atendido'],
-            datasets: [{
-                label: 'Estado de turnos',
-                data: [pendiente, cancelado, ausente, atendido],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                ],
-                borderWidth: 1
-            }]
-        });
-    }, [appointments]);
+
+
 
     return (
         <div className={Style.modal_container} onClick={(e) => {
             if (e.target.className === Style.modal_container) { onClose() }
         }}>
             <div className={Style.modal} >
+                <MiniNavBar miniTitle={"Estado de los turnos"} btnClose={true} close={onClose} /> 
                 <div className={Style.item}>
-                    {/* <MIniNavBar miniTitle={""} btnClose={true} close={onClose} /> */}
-                    <div className={Style.close} ><BtnClose close={onClose} /></div>
-                    <div>
-                        <Bar data={data} />
+                    
+                    {/* <div className={Style.close} ><BtnClose close={onClose} /></div> */}
+                    <div className={Style.pieChart}>
+                        <Pie
+                            // ref={chartRef}
+                            
+                            type="pie"
+                            data={data}
+                            options={options}
+                        >
+                        </Pie>
                     </div>
                 </div>
             </div>
